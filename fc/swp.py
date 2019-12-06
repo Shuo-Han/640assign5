6edd2dba9314c19990fd22a78607fa64f889c201
shan238 <shan238@wisc.edu>
2019-12-06 15:21:42 -0600

    a

--- fc/swp.py
+++ fc/swp.py
@@ -105,12 +105,13 @@ class SWPSender:
         # TODO
         packet = SWPPacket(SWPType.DATA, seq_num, self.data)
         packet_byte = packet.to_bytes()
+        seq_tail = seq_num + len(self.data)
         logging.debug("data being retry %s" % self.data)
+        logging.debug("seq.tail %s" % seq.tail)
         self.timers[seq_tail]\
             = threading.Timer(SWPSender._TIMEOUT, self._retransmit, [seq_num])
         self.timers[seq_tail].start()
         self._llp_endpoint.send(packet_byte)
-        seq_tail = seq_num + len(self.data)
         return
 
     def _recv(self):
